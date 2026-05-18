0cf016c1381e62bab5d79f82580e730cb6f892cf
aanyamishra9503 <aa.nyamishra9503@gmail.com>
2026-05-18 06:23:55 +0530

    added index.py for youtube

---
+++ Index1.py
@@ -0,0 +1,11 @@
+'''import random
+num1= input("enter user number")
+num2= random.randint(1,100)
+
+if num1 >= num2:
+    print("user number is greater")
+
+else:
+    print("user number smaller")'''
+
+
